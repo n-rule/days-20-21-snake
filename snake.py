0e7235056702b2bc812bcb129ac5750018029092
n-rule <rule@ukr.net>
2021-01-26 23:23:59 +0200

    1

--- snake.py
+++ snake.py
@@ -25,7 +25,7 @@ class Snake:
         self.segments.append(square)
 
     def extend(self):
-        self.add_segment(self.segments[-1].position)
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
