d3062d516448adecb9b80d4c5121d15ce400bc63
n-rule <rule@ukr.ney>
2021-01-27 18:51:06 +0200

    1

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-    #detect collision with tail - DOESNT WORK
+    #detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
