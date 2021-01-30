6d3bb88c13b1d02366de44e8791e12615655025b
n-rule <rule@ukr.ney>
2021-01-30 14:52:53 +0200

    1

--- scoreboard.py
+++ scoreboard.py
@@ -5,7 +5,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt') as file:
+            self.high_score = file.read()
         self.hideturtle()
         self.penup()
         self.goto(0, 260)
@@ -22,8 +23,10 @@ class Scoreboard(Turtle):
                    font=("Courier", 20, "bold"))
 
     def reset(self):
-        if self.score > self.high_score:
+        if self.score > int(self.high_score):
             self.high_score = self.score
+            with open('data.txt', mode='w') as file:
+                file.write(str(self.score))
         self.score = 0
         self.refresh_scoreboard()
 
